b42282b41754ab59ccc1beb14eaff16f8985360e
Emmanuel I. Obi <withtwoemms@gmail.com>
2016-01-09 02:53:58 -0500

    console log generated url

--- js/tweetcatcher.js
+++ js/tweetcatcher.js
@@ -1,10 +1,12 @@
 $('#get-access-tokens').click(function(event){
     event.preventDefault();
     
+    url = window.location.href; 
+    url = url.substring(0, url.length - 5)  
+ 
     $.ajax({
-        type: "POST",
-        url: "",
-        data: ""
+        type: "GET",
+        url: url + "/oauth",
     }).done(function(response) {
         console.log(response);
     }); 
